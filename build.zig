8c83bca3bd1607156b3253e7f6756fe9f84c544c
Will Pragnell <wpragnell@gmail.com>
2025-11-22 21:57:19 -0800

    remove pointless test step from build.zig

--- build.zig
+++ build.zig
@@ -10,15 +10,6 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("pcm.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
-
     const example_mod = b.createModule(.{
         .root_source_file = b.path("example/main.zig"),
         .target = target,
